d5c46293a03403b29354ea606e6a286bd8f4fd96
DG <gchakradhar72@gmail.com>
2021-07-20 23:09:42 +0530

    done

--- public/download.js
+++ public/download.js
@@ -1,12 +1,15 @@
-let files;
+fetch('files', { method: 'GET' })
+	.then(response => response.json())
+	.then(toHyperLinks);
 
-fetch('files', { method: 'GET' }).then(response => response.json()).then(printAsLink);
-
-function printAsLink(files) {
-	files.forEach(file => {
+function toHyperLinks(files) {
+	files.forEach((file) => {
 		let link = document.createElement('a');
+
 		link.href = `/outgoing/${file}`;
 		link.innerText = decodeURI(file);
+		link.download = decodeURI(file);
+
 		document.body.append(link);
 	});
 }
